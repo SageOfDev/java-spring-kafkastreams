59d45696727fe5b717616e61a6caa72284c2757e
승종 배 <succeeding@nexon.co.kr>
2024-05-29 17:31:13 +0900

    fix: KafkaStreamsConfig 주석 제거

--- src/main/java/org/example/javaspringstreams/KafkaStreamsConfig.java
+++ src/main/java/org/example/javaspringstreams/KafkaStreamsConfig.java
@@ -19,29 +19,14 @@ import static org.apache.kafka.streams.StreamsConfig.*;
 @EnableKafkaStreams
 public class KafkaStreamsConfig {
 
-//    @Value(value = "${spring.application.name}")
-//    private String applicationId;
-//    @Value(value = "${spring.kafka.bootstrap-servers}")
-//    private String bootstrapServerAddresses;
-//    @Value(value = "${spring.kafka.application-server}")
-//    private String applicationServerAddress;
-//    @Value(value = "${spring.kafka.streams.state.dir}")
-//    private String stateStoreLocation;
-
     @Bean(name = KafkaStreamsDefaultConfiguration.DEFAULT_STREAMS_CONFIG_BEAN_NAME)
     KafkaStreamsConfiguration kStreamsConfig(KafkaProperties kafkaProperties) {
-//        HashMap<String, Object> props  =new HashMap<>();
         Map<String, Object> props = kafkaProperties.buildStreamsProperties(null);
         props.putAll(kafkaProperties.buildConsumerProperties(null));
         props.putAll(kafkaProperties.buildProducerProperties(null));
-//        props.put(APPLICATION_ID_CONFIG, applicationId);
-//        props.put(BOOTSTRAP_SERVERS_CONFIG, bootstrapServerAddresses);
-//        props.put(APPLICATION_SERVER_CONFIG, applicationServerAddress);
+
         props.put(DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
         props.put(DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
-//        props.put(STATE_DIR_CONFIG, stateStoreLocation);
-
-
 
         return new KafkaStreamsConfiguration(props);
     }
